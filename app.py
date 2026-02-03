973ddc24a23e8ff5a952d79ecd364b1bb58e5892
eun047 <chaeeun.wonchoi@gmail.com>
2026-02-03 23:27:28 +0900

    fix: 메뉴 JSON 파일 경로 및 파일명 오류 수정

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import json
 
-with open("data/korea.json", "r", encoding="utf-8") as f:
+with open("data/Korean.json", "r", encoding="utf-8") as f:
     data = json.load(f)
 
 menus = data["menus"]
